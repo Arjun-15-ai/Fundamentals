958289ffa71db57b0954e1c14b516c6036902e0d
Arjun <nithyaarjun1502@gmail.com>
2025-07-09 21:41:29 +0530

    Add files via upload

---
+++ Basics/Company.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Company
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String companyName = sc.nextLine();
+		String companyLocation = sc.nextLine();
+		System.out.println(companyName + " Technologies "+ companyLocation);
+	}
+}
